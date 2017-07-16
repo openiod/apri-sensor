93870a565e249fdf71a42d5f6d35b9e5d4b5d307
awiel <awiel@scapeler.com>
2017-07-16 12:35:38 +0200

    agent save usb rules

--- apri-agent/apri-agent.js
+++ apri-agent/apri-agent.js
@@ -230,11 +230,14 @@ var save99UsbSerialRules	= function() {
 	var content = '';
 	var file = '/etc/udev/rules.d/99-usb-serial.rules';
 	if (unit.id == '00000000b7e92a99' || unit.id == '000000004659c5bc') {  //'s-Gravenpolder  2e voor test
+		console.log('save usb rules for unit ' + unit.id);
 		content = 
 			'SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="AL02V14T", SYMLINK+="ttyDylos1100", MODE:="0666" \n' + 
 		 	'SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="AJ03KNV9", SYMLINK+="ttyDylos1700", MODE:="0666" \n' +
  			'SUBSYSTEM=="tty", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", SYMLINK+="ttyArduinoNano", MODE:="0666" \n';
 		fs.writeFileSync(file, content);
+		console.log('     usb rules for unit ' + unit.id + ' saved. ');
+
 	}
 }
 

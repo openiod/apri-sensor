576fbf2f2dc6b4940e92f98aef7b9eb5b3fd13a1
awiel <awiel@scapeler.com>
2017-07-17 13:20:18 +0200

    agent automate dylos sensor services

--- apri-sensor-dylos/apri-sensor-dylos.js
+++ apri-sensor-dylos/apri-sensor-dylos.js
@@ -69,11 +69,15 @@ var serialPortPath;
 
 var deviceParam			= process.argv[2];
 console.log('Param for serial device is ' + deviceParam); 
+var sensorKey			= '';
 if (deviceParam != undefined) {
 	serialPortPath		= deviceParam;
+	sensorKey			= serialPortPath.substring(8);  // minus '/dev/tty'
+	console.log('SensorKey = ' + sensorKey);
 } else {
 	serialPortPath		= "/dev/ttyUSB0";
 }
+
 
 //var serialPortPath		= "/dev/cu.usbmodem1411";
 //var serialPortPath		= "/dev/cu.usbserial-A1056661";

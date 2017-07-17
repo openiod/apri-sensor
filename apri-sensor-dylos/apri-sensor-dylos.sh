224536f7a33b67d9a687266bc61ce7a8747e7897
awiel <awiel@scapeler.com>
2017-07-17 12:43:00 +0200

    agent save sensor services

--- apri-sensor-dylos/apri-sensor-dylos.sh
+++ apri-sensor-dylos/apri-sensor-dylos.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# param 2 /$2 : comname for serial read Dylos sensor e.g. ttyDC1700 (excl. /dev/)
 cd `dirname $0`
-node apri-sensor-dylos.js >>$1 2>>$1
+node apri-sensor-dylos.js $2 >>$1 2>>$1
 exit -1

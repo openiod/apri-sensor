fdbf5608d34abbbda720e373f78847a561d72f33
awiel <awiel@scapeler.com>
2017-07-17 13:02:24 +0200

    agent automate dylos sensor services

--- apri-agent/apri-sensor-update.sh
+++ apri-agent/apri-sensor-update.sh
@@ -10,8 +10,8 @@ git pull >> ../log/apri-agent-update.log
 
 # do not restart agent because loop will occur
 #sudo systemctl restart SCAPE604-apri-agent.service
-sudo systemctl stop SCAPE604-apri-sensor-dylos.service
-sudo systemctl start SCAPE604-apri-sensor-dylos.service
+#sudo systemctl stop SCAPE604-apri-sensor-dylos.service
+#sudo systemctl start SCAPE604-apri-sensor-dylos.service
 echo End of update proces >> ../log/apri-agent-update.log
 
 exit 0

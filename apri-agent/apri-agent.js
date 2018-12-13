6d63ccfa43fd12f65977a10f75028fb527223a95
awiel <awiel@scapeler.com>
2018-12-13 22:30:40 +0100

    mics6814 def

--- apri-agent/apri-agent.js
+++ apri-agent/apri-agent.js
@@ -247,7 +247,7 @@ var save99UsbSerialRules	= function() {
 	}
 	if (unit.id == '00000000b7710419') { // || unit.id == '0000000098e6a65d') {  // Purmerend / Aalten
 		console.log('save usb rules for unit ' + unit.id);
-		content =
+			'SUBSYSTEM=="tty", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", SYMLINK+="ttyCOMBI2_MICS6814", MODE:="0666" \n' +
 			'SUBSYSTEM=="tty", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0042", SYMLINK+="ttyArduinoMega", MODE:="0666" \n';
      // ATTRS{serial}=="Arduino__www.arduino.cc__0042_556393135333519071C2"
 		fs.writeFileSync(file, content);
@@ -281,7 +281,8 @@ var saveSystemServices	= function() {
 	if (unit.id == '00000000b7710419') {
 		disableServices('apri-sensor-combi-1','');
 		disableServices('apri-sensor-combi2','');
-		createService('apri-sensor-combi2','COMBI2');
+		createService('apri-sensor-combi2','ArduinoMega');
+		createService('apri-sensor-combi2','COMBI2_MICS6814');
 	}
 	if (unit.id == '000000004659c5bc') {
 		disableServices('apri-sensor-combi-1','');

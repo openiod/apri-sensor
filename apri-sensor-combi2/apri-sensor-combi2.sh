bfd652a98d0ebd100269079a6d5303dd7f488b29
awiel <awiel@scapeler.com>
2018-12-13 23:24:25 +0100

    mics6814 def

--- apri-sensor-combi2/apri-sensor-combi2.sh
+++ apri-sensor-combi2/apri-sensor-combi2.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# param 2 /$2 : comname for serial read sensor e.g. /dev/ttyDC1700 
 cd `dirname $0`
-node apri-sensor-combi2.js >>$1 2>>$1
+node apri-sensor-combi2.js $2 >>$1 2>>$1
 exit -1

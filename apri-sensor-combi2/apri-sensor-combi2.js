7d6cdf7be1ba4f1657cf4eef433219a42a1ebfae
awiel <awiel@scapeler.com>
2019-02-13 11:42:21 +0100

    arduino mega zld

--- apri-sensor-combi2/apri-sensor-combi2.js
+++ apri-sensor-combi2/apri-sensor-combi2.js
@@ -340,6 +340,9 @@ var writeResults	= function(sensorId, measureTime, loopTime) {
 	var data			= {};
 	data.neighborhoodCode	= 'BU04390603'; //geoLocation.neighborhoodCode;
 	data.foi				= 'SCRP' + unit.id;
+	if (sensorKey != '') {
+		data.foi	+= '_' + sensorKey;
+	}
 	data.neighborhoodName	= '..'; //geoLocation.neighborhoodName;
 	data.cityCode			= 'GM0439'; //geoLocation.cityCode;
 	data.cityName			= '..'; //geoLocation.cityName;

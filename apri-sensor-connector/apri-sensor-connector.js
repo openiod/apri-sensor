6d578057d1d9f763fcf9d3c662f2734aa649d2e9
awiel <awiel@scapeler.com>
2019-05-01 23:18:19 +0200

    connector

--- apri-sensor-connector/apri-sensor-connector.js
+++ apri-sensor-connector/apri-sensor-connector.js
@@ -113,11 +113,13 @@ var dateString = today.getFullYear() + "-" + (today.getMonth()+1) + "-" +  today
 var processDataCycle	= function() {
 	setTimeout(processDataCycle, loopTimeCycle);
 
-  redisSortAsync('new', 'alpha','limit',0,5,'asc')  
+  redisSortAsync('new', 'alpha','limit',0,5,'asc')
     .then(function(res) {
       var _res = res;
+      var  result = res;
     //	redisSaddAsync('ds18b20', timeStamp.toISOString()+':ds18b20')
-      console.log('new ', _res);
+      console.log(_res);
+      console.log(_res[0]);
     });
 /*
 	counters.busy = true;

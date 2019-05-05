120503658926c105091b60d03c257c48b45e8245
awiel <awiel@scapeler.com>
2019-05-05 23:16:59 +0200

    redis debug step1

--- apri-sensor-connector/apri-sensor-connector.js
+++ apri-sensor-connector/apri-sensor-connector.js
@@ -247,6 +247,7 @@ var sendData = function(redisKey,url) {
             //console.log('Redis smove(d) from new to old-set success')
           });
         } else {
+          console.dir(response);
           console.log(response.statusCode + ' / ' + response.headers['content-type'] + ' / ' +response.body);
         }
       })

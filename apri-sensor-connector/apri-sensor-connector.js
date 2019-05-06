18d5779de8f6882df7b127fde26cb8b87e01ebec
awiel <awiel@scapeler.com>
2019-05-06 08:11:52 +0200

    axios

--- apri-sensor-connector/apri-sensor-connector.js
+++ apri-sensor-connector/apri-sensor-connector.js
@@ -28,16 +28,10 @@ var initResult 							= apriConfig.init(systemModuleFolderName+"/"+systemModuleN
 // **********************************************************************************
 
 // add module specific requires
-//var request 								= require('request');
-//var express 								= require('express');
 var axios 						  		= require('axios');
 var fs 											= require('fs');
 var redis										= require("redis");
-//const ByteLength 						= require('@serialport/parser-byte-length')
 var io	 										= require('socket.io-client');
-//const exec 									= require('child_process').exec;
-//const execFile							= require('child_process').execFile;
-
 var redisClient 						= redis.createClient();
 const {promisify} 					= require('util');
 //const redisGetAsync 				= promisify(redisClient.get).bind(redisClient);
@@ -63,59 +57,17 @@ redisClient.on("error", function (err) {
 // **********************************************************************************
 		/* web-socket */
 var socketUrl, socketPath;
-//		socketUrl 	= 'http://localhost:3010';
-//		socketUrl 	= 'http://openiod.org';
-//		socketUrl 	= 'https://openiod.org';
-//		socketUrl 	= '';
-//		socketPath	= apriConfig.urlSystemRoot + '/socket.io';
 
 //prod:
 socketUrl 									= 'https://openiod.org'; socketPath	= '/'+apriConfig.systemCode + '/socket.io';
-		//console.log(apriConfig);
-//test:
-//		socketPort	= 3010; socketUrl 	= ':'+socketPort;
-//		socketPath	= apriConfig.urlSystemRoot + '/socket.io';
-
 console.log('web-socket url: '+socketUrl+socketPath);
-
-//const port 					= new SerialPort('/dev/ttyS0')
 
 var secureSite 			= true;
 var siteProtocol 		= secureSite?'https://':'http://';
 var openiodUrl			= siteProtocol + 'aprisensor-in.openiod.org';
 var loopTimeCycle		= 5000; //ms, 20000=20 sec
 
-var usbPorts			= [];
-
-var serialPortPath;
-
-var devicesFolder = undefined;  // DS18B20 devices
-
 var unit				= {};
-
-var loopStart;
-var loopTime			= 0; // ms
-
-var sensors				= {};
-
-//var nrOfChannels		= 15;
-//var channelMaxValue;
-
-//var channelTreshold		= [];
-//for (var i=0;i<nrOfChannels;i++) {
-//	channelTreshold.push(i*5000)
-//}
-//channelMaxValue			= channelTreshold[nrOfChannels-1];
-
-// create headers to only use ones in the result files
-//var writeHeaders		= true;
-//var headerRaw			= 'dateiso;pm25;pm10\n';
-
-//var sensorFileName 		= 'sensor-raspi-result';
-//var sensorFileExtension	= '.csv';
-//var sensorLocalPathRoot = systemFolderParent + '/sensor/';
-//var fileFolder 			= 'openiod-v1';
-//var resultsFolder 		= sensorLocalPathRoot + fileFolder + "/" + 'results/';
 
 var today				= new Date();
 var dateString = today.getFullYear() + "-" + (today.getMonth()+1) + "-" +  today.getDate() + "_" + today.getHours(); // + ":" + today.getMinutes();
@@ -126,50 +78,14 @@ var processDataCycle	= function(parm) {
     setTimeout(processDataCycle, loopTimeCycle);
   }
   redisSortAsync('new', 'alpha','limit',0,1,'asc')
-    .then(function(res) {
-      var _res = res;
-      var  result = res;
-    //	redisSaddAsync('ds18b20', timeStamp.toISOString()+':ds18b20')
-      if (_res.length>0) {
-        console.log(_res);
-        console.log(_res[0]);
-        getRedisData(_res[0]);
-//        sendData(_res[0]);
-      }
-    });
-/*
-	counters.busy = true;
-	console.log('Counters pms: '+ counters.pms.nrOfMeas + '; bme280: '+ counters.bme280.nrOfMeas + '; ds18b20: '+ counters.ds18b20.nrOfMeas );
-
-  results.pms.pm1CF1							= Math.round((counters.pms.pm1CF1/counters.pms.nrOfMeas)*100)/100;
-	results.pms.pm25CF1							= Math.round((counters.pms.pm25CF1/counters.pms.nrOfMeas)*100)/100;
-	results.pms.pm10CF1							= Math.round((counters.pms.pm10CF1/counters.pms.nrOfMeas)*100)/100;
-	results.pms.pm1amb							= Math.round((counters.pms.pm1amb/counters.pms.nrOfMeas)*100)/100;
-	results.pms.pm25amb							= Math.round((counters.pms.pm25amb/counters.pms.nrOfMeas)*100)/100;
-	results.pms.pm10amb							= Math.round((counters.pms.pm10amb/counters.pms.nrOfMeas)*100)/100;
-	results.pms.part0_3							= Math.round((counters.pms.part0_3/counters.pms.nrOfMeas)*100)/100;
-	results.pms.part0_5							= Math.round((counters.pms.part0_5/counters.pms.nrOfMeas)*100)/100;
-	results.pms.part1_0							= Math.round((counters.pms.part1_0/counters.pms.nrOfMeas)*100)/100;
-	results.pms.part2_5							= Math.round((counters.pms.part2_5/counters.pms.nrOfMeas)*100)/100;
-	results.pms.part5_0							= Math.round((counters.pms.part5_0/counters.pms.nrOfMeas)*100)/100;
-	results.pms.part10_0						= Math.round((counters.pms.part10_0/counters.pms.nrOfMeas)*100)/100;
-	results.pms.nrOfMeas						= counters.pms.nrOfMeas;
-
-	results.bme280.temperature			= Math.round((counters.bme280.temperature/counters.bme280.nrOfMeas)*100)/100;
-	results.bme280.pressure					= Math.round((counters.bme280.pressure/counters.bme280.nrOfMeas)*100)/100;
-	results.bme280.rHum							= Math.round((counters.bme280.rHum/counters.bme280.nrOfMeas)*100)/100;
-	results.bme280.nrOfMeas					= counters.bme280.nrOfMeas;
-
-	results.ds18b20.temperature			= Math.round((counters.ds18b20.temperature/counters.ds18b20.nrOfMeas)*100)/100;
-	results.ds18b20.nrOfMeas				= counters.ds18b20.nrOfMeas;
-
-	initCounters();
-	counters.busy = false;
-*/
-//  sendData();
-
+  .then(function(res) {
+    var _res = res;
+    if (_res.length>0) {
+      console.log(_res[0]);
+      getRedisData(_res[0]);
+    }
+  });
 }
-
 
 var printHex = function(buffer, tekst) {
 	var str="";
@@ -227,36 +143,6 @@ var ds18b20Attributes = function(res) {
     'temperature:'+res.temperature;
 }
 
-/*
-// send data to service
-var sendData = function(redisKey,url) {
-  var _redisKey = redisKey;
-  if (url=='') {
-    // todo: problem with this redis hash so wath to do with it?
-  } else {
-    console.log(url);
-    request.get(url)
-      .on('response', function(response) {
-        console.log(response.statusCode + ' / ' + response.headers['content-type']) // 200
-        if (response.statusCode=='200') {
-          redisSmoveAsync('new','archive',_redisKey)
-          .then(function(e){
-            console.log('next');
-            processDataCycle({repeat:false}); // continue with next measurement if available
-            //console.log('Redis smove(d) from new to old-set success')
-          });
-        } else {
-          console.log(response.statusCode + ' / ' + response.headers['content-type'] + ' / ' +response.body);
-        }
-      })
-      .on('error', function(err) {
-        console.log(err)
-      })
-    ;
-  }
-};
-*/
-
 // send data to service
 var sendData = function(redisKey,url) {
   var _redisKey = redisKey;
@@ -281,18 +167,13 @@ var sendData = function(redisKey,url) {
    .catch(error => {
      log(error);
    });
-  //	return axios.get(url,{ headers: headers });
-
-
 };
-
 
 var socket = io(socketUrl, {path:socketPath});
 
 socket.on('connection', function (socket) {
 	var currTime = new Date();
 	console.log(currTime +': connect from '+ socket.request.connection.remoteAddress + ' / '+ socket.handshake.headers['x-forwarded-for'] || socket.handshake.address.address);
-
 });
 
 socket.on('disconnect', function() {
@@ -302,8 +183,6 @@ socket.on('disconnect', function() {
 socket.on('info', function(data) {
 	console.log('websocket info: ');
 	console.dir(data);
-	//io.sockets.emit('aireassignal', { data: data } );
-	//socket.broadcast.emit('aireassignal', { data: data } );
 });
 
 processDataCycle({repeat:true});

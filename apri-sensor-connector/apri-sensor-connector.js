ed5c9e542c9f0f3a365a5c1e801ff95a0f69b4c0
awiel <awiel@scapeler.com>
2019-05-26 11:56:30 +0200

    log

--- apri-sensor-connector/apri-sensor-connector.js
+++ apri-sensor-connector/apri-sensor-connector.js
@@ -118,7 +118,7 @@ var processDataCycle	= function(parm) {
   .then(function(res) {
     var _res = res;
     if (_res.length>0) {
-      console.log('New record available: '+_res[0]);
+      log('New record available: '+_res[0]);
       getRedisData(_res[0]);
     }
   });

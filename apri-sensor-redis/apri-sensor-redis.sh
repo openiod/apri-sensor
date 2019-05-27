e0de63e43ed68f2feb7ce9fda16a45fdd3acd6d3
awiel <awiel@scapeler.com>
2019-05-27 20:39:42 +0200

    del hash and remove from set

--- apri-sensor-redis/apri-sensor-redis.sh
+++ apri-sensor-redis/apri-sensor-redis.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-# Redis module for clienup archive 
-cd `dirname $0`
-node apri-sensor-redis.js $2 >>$1 2>>$1
+# Redis module for clienup archive
+
+SYSTEMCODE="SCAPE604"
+SYSTEMPATH="/opt"
+
+LOGFILE=$SYSTEMPATH/$SYSTEMCODE/log/apri-sensor-redis.log
+echo "Start procedure on: " `date` >>$LOGFILE
+
+mkdir -p $SYSTEMPATH/$SYSTEMCODE/log
+
+cd  $SYSTEMPATH/$SYSTEMCODE/apri-sensor
+
+/usr/bin/node apri-sensor-redis/apri-sensor-redis.js >>$LOGFILE 2>>$LOGFILE
+
 exit -1

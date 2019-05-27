4bae2e086aa13839252cb33ce647b5276af730c0
awiel <awiel@scapeler.com>
2019-05-27 19:21:35 +0200

    del hash and remove from set

--- apri-sensor-redis/apri-sensor-redis.js
+++ apri-sensor-redis/apri-sensor-redis.js
@@ -39,6 +39,8 @@ const redisHmsetHashAsync 	= promisify(redisClient.hmset).bind(redisClient);
 const redisSaddAsync 				= promisify(redisClient.sadd).bind(redisClient);
 const redisSCard     				= promisify(redisClient.scard).bind(redisClient);
 const redisSort     				= promisify(redisClient.sort).bind(redisClient);
+const redisDel       				= promisify(redisClient.del).bind(redisClient);
+const redisSRem     				= promisify(redisClient.srem).bind(redisClient);
 
 redisClient.on("error", function (err) {
     console.log("Redis client Error " + err);
@@ -56,23 +58,46 @@ var logDir = function(object){
 var maxNrOfArchiveRecords = 28000; //40000;
 log('start Redis clean-up archive');
 
-redisSCard('archive')
-.then(function(res) {
-    var nrOfArchiveRecords = res;
-    log(nrOfArchiveRecords);
-    if (nrOfArchiveRecords > maxNrOfArchiveRecords) { // 38.880 per 3 days
-      redisSort('archive','alpha','limit','0','50','asc')
+var removeHash = function(key) {
+  var _key = key;
+  redisDel(_key);
+  .then(function(res) {
+    log('key deleted '+_key+ ' ' + res);
+      redisSRem('archive',_key)
       .then(function(res) {
-        logDir(res);
-        return;
+        log('key remove from archive set '+_key+ ' ' + res);
       })
       .catch((error) => {
         log(error);
       });
-    }
-})
-.catch((error) => {
-  log(error);
-});
+  })
+  .catch((error) => {
+    log(error);
+  });
+}
+var selectKeys = function() {
+  redisSCard('archive')
+  .then(function(res) {
+      var nrOfArchiveRecords = res;
+      log(nrOfArchiveRecords);
+      if (nrOfArchiveRecords > maxNrOfArchiveRecords) { // 38.880 per 3 days
+        redisSort('archive','alpha','limit','0','50','asc')
+        .then(function(res) {
+          logDir(res);
+          for (var i=0;i<res.lenght;i++) {
+            removeHash(res[i]);
+          }
+        })
+        .catch((error) => {
+          log(error);
+        });
+      }
+  })
+  .catch((error) => {
+    log(error);
+  });
+}
+
+selectKeys();
 
 return;

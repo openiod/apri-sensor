a19209f2210288e56092df4ad9351cbddec64759
awiel <awiel@scapeler.com>
2021-02-01 20:17:04 +0100

    .

--- aprisensor-nmcli/aprisensor-nmcli-stable.sh
+++ aprisensor-nmcli/aprisensor-nmcli-stable.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #cd `dirname $0`
-cd /opt/SCAPE604/aprisensor-nmcli-stable/aprisensor-nmcli.js
+cd /opt/SCAPE604/aprisensor-nmcli-stable
 node aprisensor-nmcli-stable.js 3999 >>$1 2>>$1
 exit -1

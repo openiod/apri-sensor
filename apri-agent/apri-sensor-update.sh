978cd02ea187044bd7e9c55a1d1bbc2ad5c18697
awiel <awiel@scapeler.com>
2021-02-28 22:53:26 +0100

    shift log plus restart services

--- apri-agent/apri-sensor-update.sh
+++ apri-agent/apri-sensor-update.sh
@@ -6,7 +6,17 @@ cd ..
 echo Start of update proces >> ../log/apri-agent-update.log
 date >> ../log/apri-agent-update.log
 # get software updates from github
+git checkout package-lock.json
 git pull >> ../log/apri-agent-update.log
+
+cd /opt/SCAPE604/log
+mv SCAPE604-apri-sensor-connector-old1.log SCAPE604-apri-sensor-connector-old2.log
+mv SCAPE604-apri-sensor-connector.log SCAPE604-apri-sensor-connector-old1.log
+systemctl restart SCAPE604-apri-sensor-connector
+
+mv SCAPE604-apri-sensor-raspi-old1.log SCAPE604-apri-sensor-raspi-old2.log
+mv SCAPE604-apri-sensor-raspi.log SCAPE604-apri-sensor-raspi-old1.log
+systemctl restart SCAPE604-apri-sensor-raspi
 
 # do not restart agent because loop will occur
 #sudo systemctl restart SCAPE604-apri-agent.service

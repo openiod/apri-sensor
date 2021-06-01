dd0218ea92adde0e3e09d1b10a0403c0a925cd4e
awiel <awiel@scapeler.com>
2021-06-01 22:52:02 +0200

    ip r default first

--- aprisensor-nmcli/aprisensor-nmcli.js
+++ aprisensor-nmcli/aprisensor-nmcli.js
@@ -1236,7 +1236,7 @@ const getActiveConnection = function() {
 const statusCheck = async function() {
 
   // determine with result of ping to (default) gateway if connection is active
-  execPromise("ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null")
+  execPromise("ping -q -w 1 -c 1 `ip r | grep default | head -1 | cut -d ' ' -f 3` > /dev/null")
   .then((result)=>{
     console.log('statusCheck then: '+result)
     // console.dir(result) -> result.stderr result.stdout

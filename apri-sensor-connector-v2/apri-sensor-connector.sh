b8f5da6dba56a0e65a99b13cb6473dc8c43b04a8
awiel <awiel@scapeler.com>
2021-07-24 21:56:37 +0200

    cleanup

--- apri-sensor-connector-v2/apri-sensor-connector.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-node apri-sensor-connector.js $2 >>$1 2>>$1
-exit -1
--- apri-sensor-raspi-v1/apri-sensor-raspi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# param 2 /$2 : comname for serialport e.g /dev/ttyDylos-DC1700
-cd `dirname $0`
-node apri-sensor-raspi.js $2 >>$1 2>>$1
-exit -1

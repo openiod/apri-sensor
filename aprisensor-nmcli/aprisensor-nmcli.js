51904625c71c4bbf0cb82bdacb318b1dac934607
awiel <awiel@scapeler.com>
2021-07-27 13:57:53 +0200

    restart avahi-daemon for correct ####.local use

--- aprisensor-nmcli/aprisensor-nmcli.js
+++ aprisensor-nmcli/aprisensor-nmcli.js
@@ -945,7 +945,17 @@ var startActionReboot = function() {
 }
 
 const actions = []
-
+actions.push(function() {
+  // restart avahi-daemon for correct ####.local
+	exec("systemctl restart avahi-daemon ", (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+		} else {
+  		console.log('avahi-daemon restart') 
+    }
+    nextAction()
+	})
+})
 actions.push(function() {
 //  console.log('getHostname 1')
 	exec("hostname", (error, stdout, stderr) => {

29c83c902c1462015cf6bb58fc6ba3f7376bdbea
awiel <awiel@scapeler.com>
2021-08-09 13:13:33 +0200

    v2.0.1

--- install/git2as.sh
+++ install/git2as.sh
@@ -1,14 +1,16 @@
 #
 # script copies files from git/apri-sensor to apri-sensors
 
-# apri-agent only once
-# cp -r /opt/SCAPE604/git/apri-sensor/apri-agent /opt/SCAPE604/apri-sensor/.
-cp /opt/SCAPE604/git/apri-sensor/apri-agent/apri-sensor-update.sh /opt/SCAPE604/apri-sensor/apri-agent/.
-# apri-config only once
-# cp -r /opt/SCAPE604/git/apri-sensor/apri-config /opt/SCAPE604/apri-sensor/.
 
 if [ -d /opt/SCAPE604/git/apri-sensor ]
 then
+  # apri-agent only once
+  # cp -r /opt/SCAPE604/git/apri-sensor/apri-agent /opt/SCAPE604/apri-sensor/.
+
+  cp /opt/SCAPE604/git/apri-sensor/apri-agent/apri-sensor-update.sh /opt/SCAPE604/apri-sensor/apri-agent/.
+  # apri-config only once
+  # cp -r /opt/SCAPE604/git/apri-sensor/apri-config /opt/SCAPE604/apri-sensor/.
+
   cp -r /opt/SCAPE604/git/apri-sensor/package.json /opt/SCAPE604/apri-sensor/.
   cp -r /opt/SCAPE604/git/apri-sensor/apri-sensor-connector /opt/SCAPE604/apri-sensor/.
   cp -r /opt/SCAPE604/git/apri-sensor/apri-sensor-raspi /opt/SCAPE604/apri-sensor/.

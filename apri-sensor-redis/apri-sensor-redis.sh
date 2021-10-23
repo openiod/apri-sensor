76d0be8a88eec9486a9ff529521aabc586b28e22
awiel <awiel@scapeler.com>
2021-10-23 11:00:03 +0200

    clean logs in redis.sh crontab

--- apri-sensor-redis/apri-sensor-redis.sh
+++ apri-sensor-redis/apri-sensor-redis.sh
@@ -14,4 +14,14 @@ cd  $SYSTEMPATH/$SYSTEMCODE/apri-sensor
 
 node apri-sensor-redis/apri-sensor-redis.js >>$LOGFILE 2>>$LOGFILE
 
+rm /var/log/*gz
+rm /var/log/*.1
+
+cd /opt/SCAPE604/log
+systemctl stop SCAPE604-apri-sensor-connector
+mv SCAPE604-apri-sensor-connector-old1.log SCAPE604-apri-sensor-connector-old2.log
+mv SCAPE604-apri-sensor-connector.log SCAPE604-apri-sensor-connector-old1.log
+systemctl start SCAPE604-apri-sensor-connector
+
+
 exit

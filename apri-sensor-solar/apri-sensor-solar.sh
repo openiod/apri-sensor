40112a9133b4fd56a86ffe14a1de9f2747729150
awiel <awiel@scapeler.com>
2022-06-11 20:05:50 +0200

    solar

--- apri-sensor-solar/apri-sensor-solar.sh
+++ apri-sensor-solar/apri-sensor-solar.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # param 2 /$2 : comname for serial read solar/pyranometer apri-sensor-solar unit
+# param 3 /$3 : baudrate for serial read solar/pyranometer apri-sensor-solar unit
 cd `dirname $0`
-node apri-sensor-solar.js $2 >>$1 2>>$1
+node apri-sensor-solar.js $2 $3 >>$1 2>>$1
 exit -1

e7d1e6997a0d996ab034de5a2b20428e64b90193
awiel <awiel@scapeler.com>
2022-11-17 23:03:54 +0100

    bam1020 start plus params

--- apri-sensor-bam1020/apri-sensor-bam1020.sh
+++ apri-sensor-bam1020/apri-sensor-bam1020.sh
@@ -2,5 +2,5 @@
 
 # param 2 /$2 : comname for serial read BAM1020 monitor e.g. /dev/ttyDC1700
 cd `dirname $0`
-node apri-sensor-bam1020.js $2 >>$1 2>>$1
+node apri-sensor-bam1020.js /dev/ttyUSB0 19200 $2 >>$1 2>>$1
 exit -1

ad3239d5c10525ff5ae1f80c0be8e120db38c9bf
awiel <awiel@scapeler.com>
2022-11-21 11:05:19 +0100

    radiationd sensor

--- apri-sensor-radiationd/apri-sensor-radiationd.sh
+++ apri-sensor-radiationd/apri-sensor-radiationd.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# param 2 /$2 : comname for serial read radiationd sensor e.g. /dev/ttyUSB0
 cd `dirname $0`
-node apri-sensor-radiationd.js >>$1 2>>$1
+node apri-sensor-radiationd.js /dev/ttyUSB0 9600 >>$1 2>>$1
 exit -1
+

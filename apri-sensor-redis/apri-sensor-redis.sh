d6d5713dfd239c598c8b00d39a110dd2f1e45ca2
awiel <awiel@scapeler.com>
2023-01-14 11:22:45 +0100

    log path

--- apri-sensor-redis/apri-sensor-redis.sh
+++ apri-sensor-redis/apri-sensor-redis.sh
@@ -45,12 +45,12 @@ fi
 # opruimen logfiles zolang winston nog niet volledig ingericht is
 # ook de oude map voorlopig meenmenen (tot ApriSensorSK2)
 cd /var/log/aprisensor
-if [ -f /opt/SCAPE604/git/apri-sensor/SCAPE604-apri-sensor-connector.log ]
+if [ -f SCAPE604-apri-sensor-connector.log ]
 then
   mv SCAPE604-apri-sensor-connector-old1.log SCAPE604-apri-sensor-connector-old2.log
   mv SCAPE604-apri-sensor-connector.log SCAPE604-apri-sensor-connector-old1.log
 fi
-if [ -f /opt/SCAPE604/git/apri-sensor/SCAPE604-apri-sensor-connector-v2.log ]
+if [ -f SCAPE604-apri-sensor-connector-v2.log ]
 then
   mv SCAPE604-apri-sensor-connector-v2-old1.log SCAPE604-apri-sensor-connector-v2-old2.log
   mv SCAPE604-apri-sensor-connector-v2.log SCAPE604-apri-sensor-connector-v2-old1.log

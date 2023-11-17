42db228f773d3d9fa13af3ddd7c18f34788db9be
awiel <awiel@scapeler.com>
2023-11-17 10:56:04 +0100

    nmcli restart only when it exists

--- install/git2as.sh
+++ install/git2as.sh
@@ -22,7 +22,12 @@ then
   cp -r /opt/SCAPE604/git/apri-sensor/aprisensor-nmcli /opt/SCAPE604/apri-sensor/.
   cp -r /opt/SCAPE604/git/apri-sensor/images /opt/SCAPE604/apri-sensor/.
   cp -r /opt/SCAPE604/git/apri-sensor/apri-config/aprisensor-types /opt/SCAPE604/apri-sensor/apri-config/aprisensor-types
-  sudo systemctl restart SCAPE604-aprisensor-nmcli
+
+  if [ -f /etc/systemd/system/SCAPE604-aprisensor-nmcli.service ]
+  then
+    echo "git2as restart SCAPE604-aprisensor-nmcli"
+    systemctl restart SCAPE604-aprisensor-nmcli
+  fi
   # sudo cp -r /opt/SCAPE604/git/apri-sensor/apri-sensor-bam1020 /opt/SCAPE604/apri-sensor/.
   # sudo cp /opt/SCAPE604/git/apri-sensor/apri-config/apri-config.js /opt/SCAPE604/apri-sensor/apri-config/.
   ## niet meer voor image versie > v2.3

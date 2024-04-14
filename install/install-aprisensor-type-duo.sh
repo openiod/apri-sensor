558f53f864a30ef0d853e706001eda72837f00ed
awiel <awiel@scapeler.com>
2024-04-14 10:58:38 +0200

    install aprisensor-type duo

--- install/install-aprisensor-type-duo.sh
+++ install/install-aprisensor-type-duo.sh
@@ -5,7 +5,7 @@
 echo "install-aprisensor-type-duo.sh start"
 
 cp /opt/SCAPE604/git/apri-sensor/apri-config/aprisensor-types/aprisensor-type-duo.json /opt/SCAPE604/apri-sensor/apri-config/aprisensor-types/.
-echo 'aprisensor-type-duo' > /opt/SCAPE604/config/aprisensor-type.cfg
+printf 'aprisensor-type-gps\n' > /opt/SCAPE604/config/aprisensor-type.cfg
 
 echo "install-aprisensor-type restart SCAPE604-apri-sensor-raspi-v2"
 systemctl restart SCAPE604-apri-sensor-raspi-v2

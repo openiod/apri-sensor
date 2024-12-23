7fe357387b471a4eef5b3dc892ae7710d95595ff
awiel <awiel@scapeler.com>
2024-12-23 10:41:50 +0100

    cleanup befor start aprisensor-nmcli

--- aprisensor-nmcli/aprisensor-nmcli.sh
+++ aprisensor-nmcli/aprisensor-nmcli.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 cd `dirname $0`
+# clean up logfile and journal history
+rm /var/log/aprisensor/SCAPE604-aprisensor-nmcli.log
+journalctl --vacuum-time=30m 
 node aprisensor-nmcli.js 4000 >>$1 2>>$1
 exit -1
